7184e775bb97d26998391f5a6b9551893b3a3677
petertran <tan196.tnt@gmail.com>
2022-05-22 22:58:04 +0700

    search log

--- src/tag-search-log/tag-search-log.service.ts
+++ src/tag-search-log/tag-search-log.service.ts
@@ -4,12 +4,7 @@ import { TagSearchLog } from './entities/tag-search-log.entity';
 
 @Injectable()
 export class TagSearchLogService {
-  async findAll() {
-    const tags = await getRepository(TagSearchLog)
-      .createQueryBuilder('tag_search_log')
-      .select('DISTINCT(tag_search_log.tagName)')
-      .take(15)
-      .getMany();
-    return tags;
+   findAll() {
+    return getRepository(TagSearchLog).find();
   }
 }
