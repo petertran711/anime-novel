a3af90d746dc948389a7a4512d992755a63a0a96
petertran <tan196.tnt@gmail.com>
2022-06-15 18:49:21 +0700

    Add download file

--- src/helpers/ultils.ts
+++ src/helpers/ultils.ts
@@ -1,5 +1,6 @@
 import { randomBytes } from 'crypto';
-
+import * as fs from 'fs';
+import * as https from 'https';
 export function getReferral(length: number) {
   const code = randomBytes(length).toString('hex').toLocaleUpperCase();
   return code;
@@ -10,4 +11,26 @@ export function createUniqName(name: string): string {
     .toLowerCase()
     .replace(/[^\w ]+/g, '')
     .replace(/ +/g, '-');
+}
+
+export async function donwloadFileFromURL(url: string, path: string, fileName: string) {
+  const filePath = path + fileName;
+  const file = fs.createWriteStream(filePath);
+  const request = https.get(url);
+  request.on('response', function (response) {
+    response.pipe(file);
+    // after download completed close filestream
+    file.on('finish', () => {
+      file.close();
+      return filePath;
+    });
+    file.on('error', (err) => {
+      file.close();
+      console.log('Cannot donwload file', err.message);
+    });
+  });
+  request.on('error', function (response) {
+    // this.logger.error(`Error:: download file s3`, response.message && response.message);
+    //
+  });
 }
