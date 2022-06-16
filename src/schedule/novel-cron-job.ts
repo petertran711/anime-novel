c399abcb28ad700fd339b6b6c92e786d3138bc20
petertran <tan196.tnt@gmail.com>
2022-06-16 14:54:59 +0700

    call create novel

--- src/schedule/novel-cron-job.ts
+++ src/schedule/novel-cron-job.ts
@@ -1,14 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
-import {NovelService} from "../novel/novel.service";
+import { NovelService } from "../novel/novel.service";
 
 @Injectable()
 export class NovelCronJob {
+  constructor(private noverservice : NovelService){}
   private readonly logger = new Logger(NovelCronJob.name);
   @Interval(1000 * 60)
-  handleCron() {
+  async handleCron() {
     this.logger.debug('Called every 10 seconds');
 
-    //this.novelService.crawlNovels();
+    const a  = this.noverservice.findOne(1)
   }
 }
